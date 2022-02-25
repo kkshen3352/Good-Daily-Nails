d1b98539575a3a2463f16d9db6bd9d759ecaebad
kkshen <h73423352@gmail.com>
2022-02-25 23:51:23 +0800

    'ud_scrolltop'

--- pages/index/nav.js
+++ pages/index/nav.js
@@ -69,8 +69,8 @@ export default function Nav() {
                         />
                     </a>
                 </Link>
-                <ScrollTop />
             </div>
+            <ScrollTop />
         </nav>
     );
 }
